bc352a93c7a3455fc6bdeee9c62a530c66d1df7f
Dara Shorten <dara.shorten@gmail.com>
2019-03-25 14:06:26 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p class="App-intro">
+		Change made
+		  </p>
           <a
             className="App-link"
             href="https://reactjs.org"
